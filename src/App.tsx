dce14fab5d80e15fbceb04fec6d36bcd14d81c32
MatheusFPalma <matheusfpalma@hotmail.com>
2023-09-03 14:36:59 -0300

    cards att

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import About from "./components/About/About";
+import Card from "./components/Cards/Cards";
+import navigation from "./config/navigation";
+function App() {
+  const [selectedPersonagem, setSelectedPersonagem] = useState("");
+  const [showAbout, setShowAbout] = useState(false);
+  const [bodyBackground, setBodyBackground] = useState("");
+  useEffect(() => {
+    if (showAbout) {
+      console.log("About foi exibido!");
+    }
+  }, [showAbout]);
+  const handleCardClick = (personagem, backgroundColor) => {
+    setSelectedPersonagem(personagem);
+    setShowAbout(true);
+    setBodyBackground(backgroundColor);
+  };
+  return (
+    <div
+      style={{
+        backgroundColor: bodyBackground,
+        padding: "1rem",
+        display: "flex",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      {showAbout ? (
+        <>
+          <div>
+            {navigation.map((item) => {
+              return (
+                <Card
+                  key={item.key}
+                  titulo={item.nome}
+                  image={item.imagemCard}
+                  background={item.tema}
+                  onClick={() => handleCardClick(item.personagem, item.tema)}
+                />
+              );
+            })}
+          </div>
+          <About personagem={selectedPersonagem} />
+        </>
+      ) : (
+        <div>
+          {navigation.map((item) => {
+            return (
+              <Card
+                key={item.key}
+                titulo={item.nome}
+                image={item.imagemCard}
+                background={item.tema}
+                onClick={() => handleCardClick(item.personagem, item.tema)}
+              />
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
+export default App;
---
+++ src/data/Person.ts
@@ -0,0 +1,37 @@
+export const person = [
+  {
+    nome: "Andrea Noer",
+    descricao: "Descrição da Andrea Noer",
+    softSkills: "Qualidades",
+    img: "./public/imagens/Andrea/corpo.png",
+    backgroundColor: "blue",
+  },
+  {
+    nome: "Everton Cadona",
+    descricao: "Descrição do Everton Cadona",
+    softSkills: "Qualidades",
+    img: "./public/imagens/Everton/corpo.png",
+    backgroundColor: "red",
+  },
+  {
+    nome: "Luciano dos Reis",
+    descricao: "Descrição do Luciano dos Reis",
+    softSkills: "Qualidades",
+    img: "./public/imagens/Luciano/corpo.png",
+    backgroundColor: "green",
+  },
+  {
+    nome: "Matheus Falkenburg",
+    descricao: "descrição do Matheus Falkenburg",
+    softSkills: "Qualidades",
+    img: "./public/imagens/Mateus/corpo.png",
+    backgroundColor: "purple",
+  },
+  {
+    nome: "Thamires Lopes",
+    descricao: "descrição da Thamires Lopes",
+    softSkills: "Qualidades",
+    img: "./public/imagens/Thamires/corpo.png",
+    backgroundColor: "gray",
+  },
+];
